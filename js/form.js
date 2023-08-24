ff0dfb62fa528fb129033788c7e8ea9eafe307e6
Doha Hmeid <doha.hmeid@gmail.com>
2023-08-24 12:48:21 +0300

    solving the comments on the pull request

--- js/form.js
+++ js/form.js
@@ -1,39 +1,33 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getDatabase, onValue, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+import { db, ref, set, get, child } from "./service.js";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBZ33dDbUj7p5n8k5aQivuDyPqVx1OU1vQ",
-    authDomain: "task1---events-webpage.firebaseapp.com",
-    databaseURL: "https://task1---events-webpage-default-rtdb.firebaseio.com",
-    projectId: "task1---events-webpage",
-    storageBucket: "task1---events-webpage.appspot.com",
-    messagingSenderId: "642153429925",
-    appId: "1:642153429925:web:24b7fc5f171016582cc64c",
-    measurementId: "G-9N01SC4T9N"
-};
-
-const app = initializeApp(firebaseConfig); //To Initialize Firebase
-const db = getDatabase(app);
 const submitBtn = document.getElementById("submit-form-btn");
 const cancelBtn = document.getElementById("cancel-form-btn");
-submitBtn.disabled = true;
+const nameField = document.getElementById("event-name");
+const descriptionField = document.getElementById("event-description");
+const dateField = document.getElementById("event-date");
+
+submitBtn.disabled = true; //by default the submit button is disabled
 loadPage();
+
 
 //*****************EVENT LISTENERS*****************/
 //CANCEL button functionality
 cancelBtn.addEventListener("click", cancelEvent);
 function cancelEvent() {
-    set(ref(db, 'eventToEdit/'), { //when user clicks cancel - no events to edit so make sure eventToEdit is set to none 
-        id: "none",
-        name: "none",
-        description: "none",
-        date: "none"
+    set(ref(db, 'eventToEdit/'), { //when user clicks cancel - no events to edit so make sure eventToEdit is set to "" empty string 
+        id: "",
+        name: "",
+        description: "",
+        date: ""
     });
     window.location.href = "list.html";
 }
-document.getElementById("event-name").addEventListener("keyup", validateForm);
-document.getElementById("event-date").addEventListener("change", validateForm);
-document.getElementById("event-description").addEventListener("keyup", validateForm);
+
+//Add event listeners to form input fields to validate the form inputs
+nameField.addEventListener("keyup", validateForm);
+descriptionField.addEventListener("keyup", validateForm);
+dateField.addEventListener("change", validateForm);
+
 
 //*****************FUNCTIONS*****************/
 //Here, check if the page is to add a new event or edit an event and based on that load event listeners
@@ -44,26 +38,12 @@ function loadPage() {
             let description = `${snapshot.val().description}`;
             let date = `${snapshot.val().date}`;
             let id = `${snapshot.val().id}`;
+            let isNew = true; //initially create event mode
 
-            if (name == "none") {   //To add a new event
+            if (name == "") {   //To add a new event
                 document.getElementById("heading1").innerHTML = "Create Event";
                 document.getElementById("submit-form-btn").value = "Add";
-                submitBtn.addEventListener("click", (event) => {
-                    event.preventDefault();
-                    let eventName = document.getElementById("event-name").value;
-                    let eventDescription = document.getElementById("event-description").value;
-                    let eventDate = document.getElementById("event-date").value;
-                    let eventID = eventName + Math.floor((Math.random() * 9999) + 1);
-                    eventName = eventName.charAt(0).toUpperCase() + eventName.substr(1).toLowerCase();//convert to title case
-                    set(ref(db, 'events/' + eventID),
-                        {
-                            id: eventID,
-                            name: eventName,
-                            description: eventDescription,
-                            date: eventDate
-                        });
-                    window.location.href = "list.html";
-                });
+                isNew = true;  //means create event mode
             }
             else {   //To edit an event
                 document.getElementById("heading1").innerHTML = "Edit Event";
@@ -71,29 +51,14 @@ function loadPage() {
                 document.getElementById("event-name").value = name;
                 document.getElementById("event-description").value = description;
                 document.getElementById("event-date").value = date;
-                submitBtn.addEventListener("click", (event) => {
-                    event.preventDefault();
-                    let newName = document.getElementById("event-name").value;
-                    let newDescription = document.getElementById("event-description").value;
-                    let newDate = document.getElementById("event-date").value;
-                    newName = newName.charAt(0).toUpperCase() + newName.substr(1).toLowerCase();//convert to title case
-                    set(ref(db, 'events/' + id),
-                        {
-                            id: id,
-                            name: newName,
-                            description: newDescription,
-                            date: newDate
-                        });
-                    set(ref(db, 'eventToEdit/'),
-                        {
-                            id: "none",
-                            name: "none",
-                            description: "none",
-                            date: "none"
-                        });
-                    window.location.href = "list.html";
-                });
+                isNew = false;  //means edit event mode
             }
+
+            submitBtn.addEventListener("click", (event) => {
+                event.preventDefault();
+                submitForm(isNew, id);
+            });
+
         } else {
             console.log("No data available");
         }
@@ -102,31 +67,40 @@ function loadPage() {
     });
 }
 
-//This function is used to validate form inputs
+//This function is used to submit the form data to the firebase database
+function submitForm(isNew, id) {
+    let eventName = document.getElementById("event-name").value;
+    let eventDescription = document.getElementById("event-description").value;
+    let eventDate = document.getElementById("event-date").value;
+    eventName = eventName.charAt(0).toUpperCase() + eventName.substr(1).toLowerCase();//convert to title case
+
+    var eventID = id;
+    if (isNew == true) { //create a new event mode
+        eventID = eventName + Math.floor((Math.random() * 9999) + 1); //create a new id for the new event
+    }
+    else { //edit event mode
+        set(ref(db, 'eventToEdit/'),
+            {
+                id: "",
+                name: "",
+                description: "",
+                date: ""
+            });
+    }
+    set(ref(db, 'events/' + eventID),
+        {
+            id: eventID,
+            name: eventName,
+            description: eventDescription,
+            date: eventDate
+        });
+    window.location.href = "list.html";
+}
+
+//This function is used to validate form inputs (name and date)
 function validateForm() {
-    console.log("i am in validation");
     let name = document.getElementById("event-name").value;
     let date = document.getElementById("event-date").value;
-    if (name && date) {
-        if (name == null || name == "" || name == " ") { //|| name.trim()==""
-            //alert("Name can't be blank");
-            submitBtn.disabled = true;
-            return false;
-        }
-        else if (name.length < 3) {
-            //alert("Name must be at least 3 characters");
-            submitBtn.disabled = true;
-            return false;
-        }
-        else {
-            //alert('Valid name given.');
-            submitBtn.disabled = false;
-            return true;
-        }
-    }
-    else {
-        //alert('Empty name or date given.');
-        submitBtn.disabled = true;
-        return false;
-    }
+    //if any of the conditions below are true then the button should be disabled
+    submitBtn.disabled = (date === null || date === "" || name === null || name === "" || name.trim() === "" || name.length < 3);
 }
--- js/list.js
+++ js/list.js
@@ -1,60 +1,65 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
-import { getDatabase, onValue, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
+import { db, app, getDatabase, ref, set, get, child } from "./service.js";
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBZ33dDbUj7p5n8k5aQivuDyPqVx1OU1vQ",
-    authDomain: "task1---events-webpage.firebaseapp.com",
-    databaseURL: "https://task1---events-webpage-default-rtdb.firebaseio.com",
-    projectId: "task1---events-webpage",
-    storageBucket: "task1---events-webpage.appspot.com",
-    messagingSenderId: "642153429925",
-    appId: "1:642153429925:web:24b7fc5f171016582cc64c",
-    measurementId: "G-9N01SC4T9N"
-};
-
-const app = initializeApp(firebaseConfig); //To initialize Firebase
-const db = getDatabase(app);
 const newBtn = document.getElementById("new-event-btn");
 const searchBar = document.getElementById("searchBar");
-const list = document.getElementById("sort-options");
+const sortlist = document.getElementById("sort-options");
 
+var originalArray = []; //this array contains the original data from the firebase database
+var filteredArray = []; //this array contains filtered group of data from the originalArray
 var totalRecord = 0;
-loadEvents(); //load the events from database to the webpage
+
+loadEvents(); //load the events from database to the originalArray
 
 
 //*****************EVENT LISTENERS*****************/
 //NEW+ button functionality
 newBtn.addEventListener("click", newEvent);
 function newEvent() {
-    set(ref(db, 'eventToEdit/'), { //when user clicks new - no events to edit so make sure eventToEdit is set to none 
-        id: "none",
-        name: "none",
-        description: "none",
-        date: "none"
+    set(ref(db, 'eventToEdit/'), { //when user clicks new - no events to edit so make sure eventToEdit is set to "" empty string 
+        id: "",
+        name: "",
+        description: "",
+        date: ""
     });
     window.location.href = "form.html";
 }
 
-//Search bar functionality 
+//Search bar functionality: displays only events that match the search query
 searchBar.onkeyup = () => {
     let searchName = document.getElementById("searchBar").value.toUpperCase();
-    let cards = document.getElementsByClassName("card");
-    //iterate through all cards and hide cards that don't match the search query
-    var i;
-    for (i = 0; i < cards.length; i++) {
-        let name = cards[i].textContent.toUpperCase(); //or name = cards[i].innerText;
-        if (name.indexOf(searchName) > -1) { //check if searchName is substring of card name
-            cards[i].style.display = "";
-        } else {
-            cards[i].style.display = "none";
+    //iterate through all cards and add only cards that match the search query to the searchResultCards array
+    let searchResultCards = [];
+    for (let k = 0; k < originalArray.length; k++) {
+        let name1 = originalArray[k].name.toUpperCase();
+        if (name1.indexOf(searchName) > -1) { //check if searchName is a substring of card name
+            searchResultCards.push(originalArray[k]); //add to filteredArray
         }
     }
+    filteredArray = [...searchResultCards];
+    clearEvents();
+    displayCards();
 }
 
+
 //Sorting drop-down list functionality
-list.onchange = () => {
+sortlist.onchange = () => {
+
+    var value = sortlist.value; //date, name
+
+    if (value == "date") { //sort by date
+        filteredArray.sort(function (a, b) {
+            return new Date(b.date) - new Date(a.date); //convert date strings to date objects and then subtract them
+        });
+    }
+    else if (value == "name") { //sort by name
+        filteredArray.sort(function (a, b) {
+            var textA = a.name.toUpperCase();
+            var textB = b.name.toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        });
+    }
     clearEvents();
-    loadEvents();
+    displayCards();
 }
 
 
@@ -64,90 +69,9 @@ function loadEvents() {
     const dbRef = ref(getDatabase(app));
     get(child(dbRef, 'events/')).then((snapshot) => {
         if (snapshot.exists()) {
-
-            let arr = snapshotToArray(snapshot); //CONVERT FIREBASE DATABASE SNAPSHOT/COLLECTION TO AN ARRAY
-            var value = list.value; //date, name
-            //console.log("value" + value);
-            if (value == "date") { //sort by date
-                arr.sort(function (a, b) {
-                    return new Date(b.date) - new Date(a.date); //convert date strings to date objects and then subtract them
-                });
-            }
-            else { //sort by name
-                arr.sort(function (a, b) {
-                    var textA = a.name.toUpperCase();
-                    var textB = b.name.toUpperCase();
-                    return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-                });
-            }
-            console.log("Array after sorting: ");
-            console.log(arr);
-
-            let i = 0;
-            while (i < arr.length) { //load the cards
-                var name = arr[i].name;
-                //name = name.charAt(0).toUpperCase() + name.substr(1).toLowerCase();//convert to title case
-                var description = arr[i].description;
-                var date = arr[i].date;
-                console.log(name);
-                console.log(description);
-                console.log(date);
-                var newDiv = document.createElement("div");
-                var nameTextNode = document.createTextNode(name);
-                var lineBreak1 = document.createElement("br");
-                var descriptionTextNode = document.createTextNode(description);
-                var lineBreak2 = document.createElement("br");
-                var dateTextNode = document.createTextNode(date);
-
-                //Past date:red - Current date:blue - Future date:Purple  
-                newDiv.setAttribute('class', 'card');
-                if (compareDates(date) == 1)
-                    newDiv.style.backgroundColor = "#982176"; //purple
-                else if (compareDates(date) == 0)
-                    newDiv.style.backgroundColor = "#1434A4"; //blue
-                else if (compareDates(date) == -1)
-                    newDiv.style.backgroundColor = "#D22B2B"; //red
-
-                newDiv.appendChild(nameTextNode);
-                newDiv.appendChild(lineBreak1);
-                newDiv.appendChild(descriptionTextNode);
-                newDiv.appendChild(lineBreak2);
-                newDiv.appendChild(dateTextNode);
-                document.getElementById("content").appendChild(newDiv);
-                i++;
-            }
-
-            //load the count value and write it on the page
-            totalRecord = arr.length;
-            console.log("Total Record : " + totalRecord);
-            document.getElementById("count-label").innerHTML = "Count: " + totalRecord;
-
-            setTimeout(function () {
-                //check for edits
-                const allCardsArray = document.getElementsByClassName("card");
-                for (var i = 0; i < allCardsArray.length; i++) {
-                    let card = allCardsArray[i];
-                    card.addEventListener('click', function () {
-                        let cardName = card.childNodes[0].nodeValue;
-                        let cardDescription = card.childNodes[2].nodeValue;
-                        let cardDate = card.childNodes[4].nodeValue;
-                        //console.log(cardName);console.log(cardDate);
-                        const c = arr.find(item => item.name == cardName);
-                        //console.log(c);
-                        //console.log(c.key);
-                        set(ref(db, 'eventToEdit/'),
-                            {
-                                id: c.key,
-                                name: cardName,
-                                description: cardDescription,
-                                date: cardDate
-                            });
-                        window.location.href = 'form.html';
-                    });
-                }
-            }, 2000);
-
-
+            originalArray = snapshotToArray(snapshot); //CONVERT FIREBASE DATABASE SNAPSHOT/COLLECTION TO AN ARRAY
+            filteredArray = [...originalArray];
+            displayCards(); //intially display the cards as the order they appear in the database
         } else {
             console.log("No data available");
         }
@@ -156,12 +80,56 @@ function loadEvents() {
     });
 }
 
+//This function is used to display the data inside the filteredArray as cards on the screen
+function displayCards() {
+    for (let i = 0; i < filteredArray.length; i++) { //load the cards
+        var name = filteredArray[i].name;
+        var description = filteredArray[i].description;
+        var date = filteredArray[i].date;
+
+        var newDiv = document.createElement("div");
+        var nameTextNode = document.createTextNode(name);
+        var lineBreak1 = document.createElement("br");
+        var descriptionTextNode = document.createTextNode(description);
+        var lineBreak2 = document.createElement("br");
+        var dateTextNode = document.createTextNode(date);
+
+        //Past date:red - Current date:blue - Future date:Purple  
+        newDiv.setAttribute('class', 'card');
+        if (compareDates(date) == 1)
+            newDiv.style.backgroundColor = "#982176"; //purple
+        else if (compareDates(date) == 0)
+            newDiv.style.backgroundColor = "#1434A4"; //blue
+        else if (compareDates(date) == -1)
+            newDiv.style.backgroundColor = "#D22B2B"; //red
+
+        newDiv.appendChild(nameTextNode);
+        newDiv.appendChild(lineBreak1);
+        newDiv.appendChild(descriptionTextNode);
+        newDiv.appendChild(lineBreak2);
+        newDiv.appendChild(dateTextNode);
+        document.getElementById("content").appendChild(newDiv);
+
+        newDiv.addEventListener('click', function () {
+            set(ref(db, 'eventToEdit/'),
+                {
+                    id: filteredArray[i].id,
+                    name: filteredArray[i].name,
+                    description: filteredArray[i].description,
+                    date: filteredArray[i].date
+                });
+            window.location.href = 'form.html';
+        });
+    }
+
+    //load the count value and write it on the page
+    totalRecord = filteredArray.length;
+    document.getElementById("count-label").innerHTML = "Count: " + totalRecord;
+}
+
 //This function deletes all the card events in the page
 function clearEvents() {
-    var allCards = document.querySelectorAll('.card');
-    allCards.forEach(c => {
-        c.remove();
-    });
+    document.getElementById("content").innerHTML = "";
 }
 
 //This function converts firebase snapshot to array
@@ -176,29 +144,25 @@ function snapshotToArray(snapshot) {
 }
 
 //This function compares one date with the current date and returns 1 if it's a future date, 0 the current date, -1 a past date
-function compareDates(divDate) {
-    const currDate = new Date();
-    //console.log("Today date"+currDate.toLocaleDateString());
-    //console.log("div date"+divDate);
+function compareDates(divDate2) {
+    let currDate = new Date(); //current date format : 8/19/2023
+    let divDate = new Date(divDate2); //convert div date format from 2023-08-31T14:40   -  type="datetime-local" to date object format
 
-    //current date format : 8/19/2023
     let currDay = currDate.getDate();
     let currMonth = currDate.getMonth() + 1;
     let currYear = currDate.getFullYear();
 
-    //div date format : 2023-08-31T14:40   -  type="datetime-local" 
-    var divDay = divDate.substring(8, 10);
-    var divMonth = divDate.substring(5, 7);
-    var divYear = divDate.substring(0, 4);
+    let divDay = divDate.getDate();
+    let divMonth = divDate.getMonth() + 1;
+    let divYear = divDate.getFullYear();
 
-    if (divYear > currYear || (divYear == currYear && divMonth > currMonth) || (divYear == currYear && divMonth == currMonth && divDay > currDay)) {
+    if (divYear > currYear || (divYear === currYear && divMonth > currMonth) || (divYear === currYear && divMonth === currMonth && divDay > currDay)) {
         return 1;
     }
-    else if (divYear == currYear && divMonth == currMonth && divDay == currDay) {
+    else if (divYear === currYear && divMonth === currMonth && divDay === currDay) {
         return 0;
     }
     else {
         return -1;
     }
-
 }
